368cb4c37563ba8ee04358e0f9ea96a30a54d9b6
Aftab Hussain <afthabhussain33@gmail.com>
2025-02-16 13:03:19 -0600

    Changed vite.cinfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,17 +11,17 @@ export default defineConfig({
     assetsDir: 'static',
     manifest: true,
   },
-  server: {
+  server: isDevelopment ? {
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
       }
     }
-  },
+  } : {},
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-})
+});
