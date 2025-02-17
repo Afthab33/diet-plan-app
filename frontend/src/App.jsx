bba2ea657721126ad4754bb82a8feade744bd016
Aftab Hussain <afthabhussain33@gmail.com>
2025-02-16 23:08:12 -0600

    Added Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import AppWrapper from './components/AppWrapper';
 import NutritionLearningCenter from './components/NutritionLearningCenter';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
+  <>
     <Routes>
       <Route path="/" element={<AppWrapper />} />
       <Route path="/nutrition-guide" element={<NutritionLearningCenter />} />
     </Routes>
+    <Analytics />
+  </> 
   );
 }
 
